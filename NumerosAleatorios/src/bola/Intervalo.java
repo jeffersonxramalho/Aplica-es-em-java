f1b1d80d74dd46f9b8cc284e12d63326ec184384
jeffersonxramalho <84602708+jeffersonxramalho@users.noreply.github.com>
2021-06-19 22:23:36 -0300

    Exercicio de numeros aleatorios
    utilizando a classe random

---
+++ NumerosAleatorios/src/bola/Intervalo.java
@@ -0,0 +1,19 @@
+package bola;
+
+import java.util.Random;
+
+public class Intervalo {
+
+	public static void main(String[] args) {
+		
+		Random aleatorio = new Random();
+		
+		 for (int i = 0; i < 5; i++) {
+	            
+			 System.out.println(aleatorio.nextInt(80));
+			 
+	        }
+
+	}
+
+}
